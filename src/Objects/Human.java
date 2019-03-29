424a6bd8cb7b203e0a318641e6d97f7cc7496ebe
asus <kemaliw@gmail.com>
2019-03-29 21:53:45 +0100

    Added Human class

---
+++ src/Objects/Human.java
@@ -0,0 +1,37 @@
+package Objects;
+
+public class Human {
+    private int age;
+    private double weight;
+    private double height;
+    private String name;
+    private boolean isMale;
+
+    public Human(int age, double weight, double height, String name, boolean isMale) {
+        this.age = age;
+        this.weight = weight;
+        this.height = height;
+        this.name = name;
+        this.isMale = isMale;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public double getWeight() {
+        return weight;
+    }
+
+    public double getHeight() {
+        return height;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public boolean isMale() {
+        return isMale;
+    }
+}
